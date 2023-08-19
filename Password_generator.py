631e215a63fd83fad0e10ba19700ff6af69b7e2b
aidin-b67 <113522563+aidin-b67@users.noreply.github.com>
2023-08-19 12:49:46 +0000

    remove one comment line

--- Password_generator.py
+++ Password_generator.py
@@ -16,7 +16,7 @@ nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
 
 #get random letter by creating loop
 random_letters = []
