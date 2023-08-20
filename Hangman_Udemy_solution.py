4293bb3a23a489752104ab6d1628348618c3951d
aidin-b67 <113522563+aidin-b67@users.noreply.github.com>
2023-08-20 11:29:58 +0000

    step 2 added

--- Hangman_Udemy_solution.py
+++ Hangman_Udemy_solution.py
@@ -6,12 +6,30 @@ word_list = ["aardvark", "baboon", "camel"]
 import random
 chosen_word = random.choice(word_list)
 
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+display =[]
+word_length = len(chosen_word)
+for _ in chosen_word:
+    display.append("_")
+print(display)
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Guess a letter: ").lower()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the leters in the chosen_word.
-for letter in chosen_word:
+# for letter in chosen_word:
+#     if letter == guess:
+#         print("Right")
+#     else:
+#         print("Wrong")
+# for step 2 we change above to the following to find position of the letter
+
+for position in range(word_length):
+    letter = chosen_word[position]
     if letter == guess:
-        print("Right")
+        display[position] = letter
     else:
-        print("Wrong")
+        display[position] = "_"
+print(display)
+
