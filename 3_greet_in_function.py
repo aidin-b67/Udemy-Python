a5e0e441774bc063a69bc7dbddc4fa73ad305518
aidin-b67 <113522563+aidin-b67@users.noreply.github.com>
2023-08-21 06:47:03 +0000

    positional argument

--- 3_greet_in_function.py
+++ 3_greet_in_function.py
@@ -8,6 +8,11 @@ def greet_with_name(name):
     print(f"Hello,{name}")
     print("to my new functiion sample")
     print("Hope you enjoy it!")
-    
+
 greet_with_name("Aidin")
-    
+
+def greet_with(name = input("What is your name?\n"),location = input("What is your location?\n")):
+    print(f"Hello,{name}")
+    print(f"to my new functiion sample in {location}")
+    print("Hope you enjoy it!")
+greet_with()
