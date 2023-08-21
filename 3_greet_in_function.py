986313265994af2a490f828c030ed6a8583cb80a
aidin-b67 <113522563+aidin-b67@users.noreply.github.com>
2023-08-21 06:31:11 +0000

    add parameter within function

--- 3_greet_in_function.py
+++ 3_greet_in_function.py
@@ -3,3 +3,11 @@ def greet():
     print("to my new functiion sample")
     print("Hope you enjoy it!")
 greet()
+
+def greet_with_name(name):
+    print(f"Hello,{name}")
+    print("to my new functiion sample")
+    print("Hope you enjoy it!")
+    
+greet_with_name("Aidin")
+    
