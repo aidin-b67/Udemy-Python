7d37d5245d445195ef55a6482743a4a38a3d3103
aidin-b67 <113522563+aidin-b67@users.noreply.github.com>
2023-08-30 15:59:17 +0000

    adding condition for calculator

--- Black-Jack-game.py
+++ Black-Jack-game.py
@@ -5,24 +5,33 @@ import random
 
 
 def deal_card():
-
+    """Return a random card from the deck"""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    return random.choice(cards)
- 
+    card = random.choice(cards)
+    return card
 
 user_cards=[]
 computer_cards=[]
 
-for card in range(0,2):
+for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-def calculator_score(user_cards, computer_cards):
-    user_cards_sum = sum(user_cards)
-    computer_cards_sum = sum(computer_cards)
+def calculator_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)
+
+
     
-print(calculator_score())
-print(f"Your cards: {user_cards}, current score: {calculator_score(user_cards_sum)}")
+    
+# sum_user = calculator_score(user_cards)
+
+print(f"Your cards: {user_cards}, current score: {calculator_score(user_cards)}")
 print(f"Computer's first card: {computer_cards[0]}")
 
 
